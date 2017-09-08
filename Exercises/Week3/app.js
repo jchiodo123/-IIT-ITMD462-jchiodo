e7d691810a030da691386d681af86134933bb277
jchiodo123 <jchiodo@hawk.iit.edu>
2017-09-08 17:53:36 -0500

    improvement #2: key listener

--- Exercises/Week3/app.js
+++ Exercises/Week3/app.js
@@ -1,18 +1,22 @@
 var main = function () {
   "use strict";
   
-  $(".comment-input button").on("click", function () {
+  $(".comment-input input").on("keypress", function (event) {
+    
+    console.log("this is the keyCode " + event.keyCode);
+    
     var $comment_text = $(".comment-input input");
     
-    if ($comment_text.val() !== "") {
-      var $new_comment = $("<p>");
-      // set the input text to the <p> 
-      $new_comment.text($comment_text.val());
-      $(".comments").append($new_comment);
-      // set val to empty, ensure the input box is cleared
-      $comment_text.val("");
+    if (event.keyCode === 13) {
+      if ($comment_text.val() !== "") {
+        var $new_comment = $("<p>");
+        // set the input text to the <p> 
+        $new_comment.text($comment_text.val());
+		    $(".comments").append($new_comment);
+		    // set val to empty, ensure the input box is cleared
+		    $comment_text.val("");
+      }
     }
   });
 };
-
 $(document).ready(main);
