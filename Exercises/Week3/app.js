519b483b60af356f2b68262ad9d770ca78897473
jchiodo123 <jchiodo@hawk.iit.edu>
2017-09-08 13:16:06 -0500

    improvement #1: clear input box

--- Exercises/Week3/app.js
+++ Exercises/Week3/app.js
@@ -2,14 +2,16 @@ var main = function () {
   "use strict";
   
   $(".comment-input button").on("click", function () {
-    var $new_comment;
-    var comment_text;
+    var $comment_text = $(".comment-input input");
     
-    comment_text = $(".comment-input input").val();
-    $new_comment = $("<p>");
-    // set the input text to the <p> 
-    $new_comment.text(comment_text);
-    $(".comments").append($new_comment);
+    if ($comment_text.val() !== "") {
+      var $new_comment = $("<p>");
+      // set the input text to the <p> 
+      $new_comment.text($comment_text.val());
+      $(".comments").append($new_comment);
+      // set val to empty, ensure the input box is cleared
+      $comment_text.val("");
+    }
   });
 };
 
