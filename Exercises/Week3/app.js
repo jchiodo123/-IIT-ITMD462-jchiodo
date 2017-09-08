ae726857f29187062fbad313c62eaf03ebea1ea9
jchiodo123 <jchiodo@hawk.iit.edu>
2017-09-08 18:40:30 -0500

    improvement #3: fade in comment

--- Exercises/Week3/app.js
+++ Exercises/Week3/app.js
@@ -1,22 +1,21 @@
 var main = function () {
   "use strict";
   
-  $(".comment-input input").on("keypress", function (event) {
-    
-    console.log("this is the keyCode " + event.keyCode);
-    
+  $(".comment-input button").on("click", function () {
     var $comment_text = $(".comment-input input");
     
-    if (event.keyCode === 13) {
-      if ($comment_text.val() !== "") {
-        var $new_comment = $("<p>");
-        // set the input text to the <p> 
-        $new_comment.text($comment_text.val());
-		    $(".comments").append($new_comment);
-		    // set val to empty, ensure the input box is cleared
-		    $comment_text.val("");
-      }
+    if ($comment_text.val() !== "") {
+      var $new_comment = $("<p>");
+      // set the input text to the <p> 
+      $new_comment.text($comment_text.val());
+      $new_comment.hide();
+      $(".comments").append($new_comment);
+      // set val to empty, ensure the input box is cleared
+      $new_comment.fadeIn(4000);
+      $comment_text.val("");
     }
   });
 };
+
 $(document).ready(main);
+
