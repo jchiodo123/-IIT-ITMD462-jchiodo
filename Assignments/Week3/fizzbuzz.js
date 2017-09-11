47b4ea4b6968a7c11ce19fdaa4cb56d519502aac
jchiodo123 <jchiodo@hawk.iit.edu>
2017-09-11 17:20:47 -0500

    assignment3 fizzbuzz, convert tabs to spaces

--- Assignments/Week3/fizzbuzz.js
+++ Assignments/Week3/fizzbuzz.js
@@ -6,36 +6,36 @@
  */
 
 var main = function () {
-	"use strict";
-
-	function fizzbuzz(first, last) {	
-		var $fizzlist = $("#fboutput");
-		
-		for (var number = first; number <= last; number++) {
-			var fbresult = "";
-			if (number % 15 === 0)
-				fbresult = "FizzBuzz";
-			else if (number % 3 === 0)
-				fbresult = "Fizz";
-			else if (number % 5 === 0)
-				fbresult = "Buzz";
-			
-			// display the result
-			if (fbresult == "") {
-				// if not fizz/buzz/fizzbuzz, append to list
-				$fizzlist.append($("<li>").text(number + " = " + number));
-				console.log(number + " = " + number);
-			} else { 
-				// if fizz/buzz/fizzbuzz, append to list and add color class
-				$fizzlist.append($("<li>").text(number + " = " + fbresult));
-				$fizzlist.find('li:last').addClass(fbresult);
-				console.log(number + " = " + fbresult);
-			}
-		
-		}
-	}
+  "use strict";
   
-	fizzbuzz(1, 100);  
+  function fizzbuzz(first, last) {	
+    var $fizzlist = $("#fboutput");
+		
+    for (var number = first; number <= last; number++) {
+      var fbresult = "";
+      if (number % 15 === 0)
+        fbresult = "FizzBuzz";
+      else if (number % 3 === 0)
+        fbresult = "Fizz";
+      else if (number % 5 === 0)
+        fbresult = "Buzz";
+			
+      // display the result
+      if (fbresult == "") {
+        // if not fizz/buzz/fizzbuzz, append to list
+        $fizzlist.append($("<li>").text(number + " = " + number));
+        console.log(number + " = " + number);
+      } else { 
+        // if fizz/buzz/fizzbuzz, append to list and add color class
+        $fizzlist.append($("<li>").text(number + " = " + fbresult));
+        $fizzlist.find('li:last').addClass(fbresult);
+        console.log(number + " = " + fbresult);
+      }
+    }
+  }
+  
+  // call fizzbuzz with range 1-100
+  fizzbuzz(1, 100);  
   
 };
 
