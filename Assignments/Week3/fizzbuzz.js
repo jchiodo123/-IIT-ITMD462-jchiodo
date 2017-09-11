90883f60b444206c147b858f288f07534b4d3831
jchiodo123 <jchiodo@hawk.iit.edu>
2017-09-11 16:07:36 -0500

    assignment3 fizzbuzz: initial commit for html/css/js

---
+++ Assignments/Week3/fizzbuzz.js
@@ -0,0 +1,42 @@
+/*
+ *	John Chiodo
+ *	jchiodo@hawk.iit.edu
+ *	ITMD462 - Assignment3 
+ *	09/12/2017
+ */
+
+var main = function () {
+	"use strict";
+
+	function fizzbuzz(first, last) {	
+		var $fizzlist = $("#fboutput");
+		
+		for (var number = first; number <= last; number++) {
+			var fbresult = "";
+			if (number % 15 === 0)
+				fbresult = "FizzBuzz";
+			else if (number % 3 === 0)
+				fbresult = "Fizz";
+			else if (number % 5 === 0)
+				fbresult = "Buzz";
+			
+			// display the result
+			if (fbresult == "") {
+				// if not fizz/buzz/fizzbuzz, append to list
+				$fizzlist.append($("<li>").text(number + " = " + number));
+				console.log(number + " = " + number);
+			} else { 
+				// if fizz/buzz/fizzbuzz, append to list and add color class
+				$fizzlist.append($("<li>").text(number + " = " + fbresult));
+				$fizzlist.find('li:last').addClass(fbresult);
+				console.log(number + " = " + fbresult);
+			}
+		
+		}
+	}
+  
+	fizzbuzz(1, 100);  
+  
+};
+
+$(document).ready(main);
